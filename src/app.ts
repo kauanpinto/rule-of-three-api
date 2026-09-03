6ee081d2bd0d78298158c560e709ca527607e468
Kauan Fernando <kauanfernando0730@gmail.com>
2026-09-02 21:43:46 -0300

    feat: add auth middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
 import type { Application, Request, Response } from 'express';
 import authRoutes from '@/routes/auth.routes';
 
@@ -9,6 +10,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
+app.use(cookieParser());
 
 app.use('/auth', authRoutes);
 
---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config';
+import jwt from 'jsonwebtoken';
+import type { Request, Response, NextFunction } from 'express';
+
+export function requireAuth(req: Request, res: Response, next: NextFunction) {
+  const token = req.cookies.token;
+
+  if (!token) {
+    res.status(401).json({ message: 'Token inválido' });
+    return;
+  }
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET!) as { userId: string };
+    (req as any).userId = payload.userId;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Token inválido' });
+    return;
+  }
+}
