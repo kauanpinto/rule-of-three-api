f077e31d6efcc0cd8571f8b94b80399eca9646a8
Kauan Fernando <kauanfernando0730@gmail.com>
2026-09-10 14:44:07 -0300

    fix: override drizzle env

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,10 @@
 import { config } from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
 
-config({ path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env' });
+config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
+  override: true,
+});
 
 export default defineConfig({
   out: './drizzle',
